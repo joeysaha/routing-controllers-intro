c8fc397375aa0cdb6e7934022a9ed4411bd44b16
Joey Saha <joeysaha@hotmail.com>
2018-08-13 15:44:10 -0400

    @header update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,14 +2,17 @@ class PagesController < ApplicationController
 
   def welcome
     # render :welcome
+    @header = "Welcome to my humble abode"
   end
 
   def about
     # render :about
+    @header = "So about me...."
   end
 
   def contest
     # render :contest
+    @header = "Did somebody say ... contest?!?!?"
   end
 
 end
